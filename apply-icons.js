15b62598f107f87a3a3a27b7d32f122e483f1bb1
Mani Kamali <mani@maniks.net>
2021-05-31 23:32:27 -0400

    Use flexbox again, stop using apply-icons.js but still use css aspect ratio hackiness

--- apply-icons.js
+++ apply-icons.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 	[...document.querySelectorAll('[data-icon]')].map(elm => {
 		elm.style.backgroundImage = `url(${elm.dataset.icon})`;
+<<<<<<< HEAD
 	});
+=======
+	}).map(console.log);
+>>>>>>> a506976 (Use flexbox again, stop using apply-icons.js but still use css aspect ratio hackiness)
 });
